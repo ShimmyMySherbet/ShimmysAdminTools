0efd7b08c6a9f9372b39cab6648f68492a396ef7
ShimmyMySherbet <ShimmyMySherbet@gmail.com>
2020-06-14 21:10:30 +1000

    Updated file info

--- ShimmysAdminTools/Properties/AssemblyInfo.cs
+++ ShimmysAdminTools/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ShimmysAdminTools")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ShimmysAdminTools")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ShimmysAdminTools")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ShimmyMySherbet©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
