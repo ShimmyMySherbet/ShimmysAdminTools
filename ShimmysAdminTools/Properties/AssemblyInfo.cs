919d2942c673831bb0a97a3ae0c2755112e13c02
ShimmyMySherbet <ShimmyMySherbet@gmail.com>
2021-04-09 14:11:32 +1000

    Update version

--- ShimmysAdminTools/Properties/AssemblyInfo.cs
+++ ShimmysAdminTools/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ShimmysAdminTools")]
 [assembly: AssemblyDescription("ShimmysAdminTools")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ShimmyMySherbet")]
 [assembly: AssemblyProduct("ShimmysAdminTools")]
 [assembly: AssemblyCopyright("Copyright ShimmyMySherbet©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
